0212a1469b5278ad53e0eaa27931c53ebec0c67e
Anuj Saxena <165511451+Anuj24s@users.noreply.github.com>
2026-04-16 09:17:40 +0530

    Change in Exp 09

--- exp_09.py
+++ exp_09.py
@@ -1,44 +1,46 @@
+# 
 # # Create vectors (data)
-# name <- c('Dinesh', 'Suresh', 'Rahul', 'Ravi', 'Manoj', 
-#           'Hari', 'Yatharth', 'Saurabh', 'Kapil', 'Salini')
+name <- c('Dinesh', 'Suresh', 'Rahul', 'Ravi', 'Manoj', 
+          'Hari', 'Yatharth', 'Saurabh', 'Kapil', 'Salini')
 
-# score <- c(12.5, 9, 16.5, NA, 9, 20, 14.5, NA, 8, 19)
+score <- c(12.5, 9, 16.5, NA, 9, 20, 14.5, NA, 8, 19)
 
-# attempts <- c(1, 3, 2, 3, 2, 3, 1, 1, 2, 1)
+attempts <- c(1, 3, 2, 3, 2, 3, 1, 1, 2, 1)
 
-# qualify <- c('yes', 'no', 'yes', 'no', 'no', 
-#              'yes', 'yes', 'no', 'no', 'yes')
+qualify <- c('yes', 'no', 'yes', 'no', 'no', 
+             'yes', 'yes', 'no', 'no', 'yes')
 
-# labels <- c('a', 'b', 'c', 'd', 'e', 'f', 
-#             'g', 'h', 'i', 'j')
+labels <- c('a', 'b', 'c', 'd', 'e', 'f', 
+            'g', 'h', 'i', 'j')
 
-# # Create DataFrame
-# exam_data <- data.frame(name, score, attempts, qualify, row.names = labels)
+# Create DataFrame
+exam_data <- data.frame(name, score, attempts, qualify, row.names = labels)
 
-# # Display DataFrame
-# print(exam_data)
+# Display DataFrame
+print(exam_data)
 
 
 #--------------------------------------------------------------------------------------
+# in python
 
-import pandas as pd
-import numpy as np
+# import pandas as pd
+# import numpy as np
 
-# Create dictionary data
-exam_data = {
-    'name': ['Dinesh', 'Suresh', 'Rahul', 'Ravi', 'Manoj',
-             'Hari', 'Yatharth', 'Saurabh', 'Kapil', 'Salini'],
-    'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
-    'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
-    'qualify': ['yes', 'no', 'yes', 'no', 'no',
-                'yes', 'yes', 'no', 'no', 'yes']
-}
+# # Create dictionary data
+# exam_data = {
+#     'name': ['Dinesh', 'Suresh', 'Rahul', 'Ravi', 'Manoj',
+#              'Hari', 'Yatharth', 'Saurabh', 'Kapil', 'Salini'],
+#     'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+#     'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+#     'qualify': ['yes', 'no', 'yes', 'no', 'no',
+#                 'yes', 'yes', 'no', 'no', 'yes']
+# }
 
-# Index labels
-labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+# # Index labels
+# labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
 
-# Create DataFrame
-df = pd.DataFrame(exam_data, index=labels)
+# # Create DataFrame
+# df = pd.DataFrame(exam_data, index=labels)
 
-# Display DataFrame
-print(df)
+# # Display DataFrame
+# print(df)
